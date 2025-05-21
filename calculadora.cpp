5e20aea9db69310987c1b266945be22685770d23
AndreyCastro28 <Andrey-Castro>
2025-05-20 19:50:50 -0600

    funcion suma terminada + incorporar gitignore

--- calculadora.cpp
+++ calculadora.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+
+
+    int suma(int a, int b) {
+        return a + b;
+    }
+    
 int main() {
 
-    
-    cout << "Hola Clase";
+
+    cout << "Hola Clase!!";
+    cout << "La suma de 1 y 2 es: " << suma(1, 2) << "!!";
     
     
     
